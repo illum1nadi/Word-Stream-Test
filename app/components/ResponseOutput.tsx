6c32e4ecbeec9a27c0be5183eb9be69a529ca9e7
illum1nadi <adityainduri@gmail.com>
2025-06-04 18:38:07 +0530

    Added support for markdown text to replicate gpt-like behaviour

--- app/components/ResponseOutput.tsx
+++ app/components/ResponseOutput.tsx
@@ -1,4 +1,8 @@
+'use client';
+
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 interface ResponseOutputProps {
   responseText: string;
@@ -6,18 +10,20 @@ interface ResponseOutputProps {
 
 export default function ResponseOutput({ responseText }: ResponseOutputProps) {
   return (
-    <pre
+    <div
       style={{
         marginTop: 20,
         padding: 10,
         backgroundColor: '#f0f0f0',
         minHeight: 100,
-        whiteSpace: 'pre-wrap',
         fontSize: 16,
+        lineHeight: 1.6,
         transition: 'opacity 0.3s ease',
       }}
     >
-      {responseText}
-    </pre>
+      <ReactMarkdown remarkPlugins={[remarkGfm]}>
+        {responseText}
+      </ReactMarkdown>
+    </div>
   );
 }
